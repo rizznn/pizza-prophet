84364def60d48596af2100aed916f01b02c03810
Riz <80712058+rizznn@users.noreply.github.com>
2021-06-20 14:58:05 -0400

    initial files and commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const pizza = {
+	pie: [
+		{
+			title: 'Maker Pizza',
+			price: '$$'
+		},
+		{
+			title: 'Pizza Libretto',
+			price: '$$'
+		},
+		{
+			title: 'Dominoes',
+			price: '$'
+		}
+	],
+	slice: [
+		{
+			title: 'Pizzaiolo',
+			price: '$$'
+		},
+		{
+			title: 'North of Brooklyn',
+			price: '$$'
+		},
+		{
+			title: 'Pizza Pizza',
+			price: '$'
+		},
+		{
+			title: 'King Slice',
+			price: '$'
+		}
+	]
+};
